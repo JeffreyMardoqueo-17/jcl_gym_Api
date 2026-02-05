b3a7589d80754fbdc59fe1bb38c8d98341164d81
Jeffrey Mardoqueo <jeffreymardoqueo260@gmail.com>
2026-02-04 20:56:08 -0600

    Prueba de integracion y despliegue nuevo enpoint

---
+++ Controllers/pruebaController.cs
@@ -0,0 +1,25 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace jcl_gym_Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PruebaController : ControllerBase
+    {
+        // GET: api/Prueba
+        [HttpGet]
+        public IActionResult Get()
+        {
+            // Configurar zona horaria de El Salvador
+            var timeZone = TimeZoneInfo.FindSystemTimeZoneById("Central Standard Time");
+            var fechaElSalvador = TimeZoneInfo.ConvertTime(DateTime.UtcNow, timeZone);
+
+            return Ok(new
+            {
+                Mensaje = "Esto es una prueba",
+                FechaElSalvador = fechaElSalvador.ToString("dd-MM-yyyy") // solo día-mes-año
+            });
+        }
+    }
+}
